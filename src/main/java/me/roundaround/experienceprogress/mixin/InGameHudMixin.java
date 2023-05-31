de139767f70d2411caa5cceb642e99173aff44e7
Evan Steinkerchner <esteinkerchner@gmail.com>
2023-05-31 18:45:41 -0400

    Update to 1.20-rc1

--- src/main/java/me/roundaround/experienceprogress/mixin/InGameHudMixin.java
+++ src/main/java/me/roundaround/experienceprogress/mixin/InGameHudMixin.java
@@ -3,8 +3,8 @@ package me.roundaround.experienceprogress.mixin;
 import me.roundaround.experienceprogress.ExperienceProgressMod;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.font.TextRenderer;
+import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.hud.InGameHud;
-import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.entity.player.PlayerEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -26,7 +26,7 @@ public abstract class InGameHudMixin {
   public abstract TextRenderer getTextRenderer();
 
   @Inject(at = @At("RETURN"), method = "renderExperienceBar")
-  public void renderExperienceProgress(MatrixStack matrixStack, int x, CallbackInfo info) {
+  public void renderExperienceProgress(DrawContext drawContext, int x, CallbackInfo info) {
     if (!ExperienceProgressMod.enabled) {
       return;
     }
@@ -44,19 +44,20 @@ public abstract class InGameHudMixin {
     int x2 = x + 182 + 2;
     int y = this.scaledHeight - 32 + 4;
 
-    this.renderNumber(matrixStack, String.valueOf(currentExperience), x1, y, true);
-    this.renderNumber(matrixStack, String.valueOf(experienceNeeded), x2, y, false);
+    this.renderNumber(drawContext, String.valueOf(currentExperience), x1, y, true);
+    this.renderNumber(drawContext, String.valueOf(experienceNeeded), x2, y, false);
   }
 
   private void renderNumber(
-      MatrixStack matrixStack, String number, int x, int y, boolean rightAligned) {
-    float renderX = x + (rightAligned ? -this.getTextRenderer().getWidth(number) : 0);
-    float renderY = y + (5 - this.getTextRenderer().fontHeight) / 2f;
+      DrawContext drawContext, String number, int x, int y, boolean rightAligned) {
+    int renderX = x + (rightAligned ? -this.getTextRenderer().getWidth(number) : 0);
+    int renderY = y + Math.round((5 - this.getTextRenderer().fontHeight) / 2f);
 
-    this.getTextRenderer().draw(matrixStack, number, renderX + 1, renderY, 0);
-    this.getTextRenderer().draw(matrixStack, number, renderX - 1, renderY, 0);
-    this.getTextRenderer().draw(matrixStack, number, renderX, renderY + 1, 0);
-    this.getTextRenderer().draw(matrixStack, number, renderX, renderY - 1, 0);
-    this.getTextRenderer().draw(matrixStack, number, renderX, renderY, 0xFFFFFF);
+    TextRenderer textRenderer = this.getTextRenderer();
+    drawContext.drawText(textRenderer, number, renderX + 1, renderY, 0, false);
+    drawContext.drawText(textRenderer, number, renderX - 1, renderY, 0, false);
+    drawContext.drawText(textRenderer, number, renderX, renderY + 1, 0, false);
+    drawContext.drawText(textRenderer, number, renderX, renderY - 1, 0, false);
+    drawContext.drawText(textRenderer, number, renderX, renderY, 0xFFFFFF, false);
   }
 }

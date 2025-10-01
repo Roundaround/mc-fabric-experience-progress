13e941c5119e6656cc37f11cbac0bb1e48186b62
Roundaround <roundaroundna@gmail.com>
2025-10-01 10:20:07 -0400

    Update to 1.21.9

--- src/main/java/me/roundaround/experienceprogress/mixin/KeyboardMixin.java
+++ src/main/java/me/roundaround/experienceprogress/mixin/KeyboardMixin.java
@@ -1,5 +1,6 @@
 package me.roundaround.experienceprogress.mixin;
 
+import net.minecraft.client.input.KeyInput;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -14,23 +15,31 @@ import net.minecraft.text.Text;
 @Mixin(Keyboard.class)
 public abstract class KeyboardMixin {
   @Shadow
-  public abstract void sendMessage(Text message);
+  protected abstract void sendMessage(Text message);
 
   @Shadow
-  public abstract void debugLog(Text message);
+  protected abstract void debugLog(Text message);
 
   @Inject(method = "processF3", at = @At(value = "RETURN"), cancellable = true)
-  private void aboveWorldRendererReload(int key, CallbackInfoReturnable<Boolean> info) {
-    if (!info.getReturnValue() && key == GLFW.GLFW_KEY_X) {
+  private void aboveWorldRendererReload(KeyInput input, CallbackInfoReturnable<Boolean> info) {
+    if (!info.getReturnValue() && input.key() == GLFW.GLFW_KEY_X) {
       ExperienceProgressMod.enabled = !ExperienceProgressMod.enabled;
-      this.debugLog(
-          Text.translatable(ExperienceProgressMod.enabled ? "experienceprogress.on" : "experienceprogress.off"));
+      this.debugLog(Text.translatable(ExperienceProgressMod.enabled ?
+          "experienceprogress.on" :
+          "experienceprogress.off"));
       info.setReturnValue(true);
     }
   }
 
-  @Inject(method = "processF3", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/Keyboard;sendMessage(Lnet/minecraft/text/Text;)V", shift = At.Shift.AFTER, ordinal = 13))
-  private void onDebugHelp(int key, CallbackInfoReturnable<Boolean> info) {
+  @Inject(
+      method = "processF3", at = @At(
+      value = "INVOKE",
+      target = "Lnet/minecraft/client/Keyboard;sendMessage(Lnet/minecraft/text/Text;)V",
+      shift = At.Shift.AFTER,
+      ordinal = 13
+  )
+  )
+  private void onDebugHelp(KeyInput input, CallbackInfoReturnable<Boolean> info) {
     this.sendMessage(Text.translatable("experienceprogress.debug.help"));
   }
 }
